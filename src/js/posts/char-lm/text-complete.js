ee905072664be0439243b7be58d3480bceb3305f
batzner <github@kilians.net>
2017-10-13 10:58:15 +0200

    Fixed char-lm text-complete URL

--- src/js/posts/char-lm/text-complete.js
+++ src/js/posts/char-lm/text-complete.js
@@ -117,7 +117,7 @@ function completeText() {
     const defaultParams = DATASET_DEFAULTS[selectedDataset];
     $.get('https://app-1506526767.000webhostapp.com',
         {
-            url: 'http://ec2-35-167-199-162.us-west-2.compute.amazonaws.com:5000/sample',
+            url: 'http://ec2-54-200-34-84.us-west-2.compute.amazonaws.com:5000/sample',
             prime: prime,
             steps: defaultParams.steps,
             maxSentences: defaultParams.maxSentences,
