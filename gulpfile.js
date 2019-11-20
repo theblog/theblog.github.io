ccaa202079a1b6b028840a3a75017092a9127848
Kilian Batzner <git@kilians.net>
2019-11-20 15:03:05 +0100

    Run jekyll build before minifying

--- gulpfile.js
+++ gulpfile.js
@@ -2,73 +2,95 @@
 
 /**
  * Basic concept follows https://www.justinmccandless.com/post/a-tutorial-for-getting-started-with-gulp/
- * TODO: Plug-ins to consider: gulp-changed, gulp-minify-html (see
- * https://www.sitepoint.com/introduction-gulp-js/)
  */
 const gulp = require('gulp');
 
 // Include plug-ins
 const eslint = require('gulp-eslint');
-const clean = require('gulp-clean');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
-const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
+const minifyCSS = require('gulp-minify-css');
+const minifyInline = require('gulp-minify-inline');
+const spawn = require('child_process').spawn;
 
-// Base for specifying paths in gulp.src and gulp.dest
-const distDir = '_dist/';
+// Build directory
+const buildDir = '_site/';
 
 // Paths to be used in gulp.src
 const sourcePaths = {
     javascripts: ['js/**/*.js', '!js/lib/**/*.js'],
-    images: ['images/**/*.png', 'images/**/*.jpg', 'images/**/*.jpeg', 'images/**/*.gif']
+    images: ['images/**/*.png', 'images/**/*.jpg', 'images/**/*.jpeg', 'images/**/*.gif'],
+    html: ['**/*.html'],
+    css: ['css/**/*.css']
 };
 
-// Delete the dist directory
-gulp.task('_clean', function () {
-    return gulp.src(distDir)
-        .pipe(clean());
+function waitForCommand(command, gulpCallBack) {
+    command.on('exit', function (code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: command exited with code: ' + code);
+    });
+}
+
+// Delete the build directory
+gulp.task('jekyll-clean', function (gulpCallBack) {
+    let command = spawn('jekyll', ['clean'], {stdio: 'inherit'});
+    waitForCommand(command, gulpCallBack);
 });
 
-// Copy all files
-gulp.task('_copy', ['_clean'], function () {
-    return gulp.src(['src/**/*']).pipe(gulp.dest(distDir));
+// Let Jekyll build the raw site
+gulp.task('jekyll-build', ['jekyll-clean'], function (gulpCallBack) {
+    let command = spawn('jekyll', ['build'], {stdio: 'inherit'});
+    waitForCommand(command, gulpCallBack);
+});
+
+// Let Jekyll serve the raw site
+gulp.task('jekyll-serve', ['jekyll-clean'], function (gulpCallBack) {
+    let command = spawn('jekyll', ['serve'], {stdio: 'inherit'});
+    waitForCommand(command, gulpCallBack);
 });
 
 // Transpile the javascript files to ES5.
-gulp.task('_javascripts', ['_copy'], function () {
-    return gulp.src(sourcePaths.javascripts, {base: distDir, cwd: distDir})
+gulp.task('_javascripts', ['jekyll-build'], function () {
+    return gulp.src(sourcePaths.javascripts, {base: buildDir, cwd: buildDir})
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest(distDir));
+        .pipe(gulp.dest(buildDir));
 });
 
 // Minify the images
-gulp.task('_images', ['_copy'], function () {
-    return gulp.src(sourcePaths.images, {base: distDir, cwd: distDir})
+gulp.task('_images', ['jekyll-build'], function () {
+    return gulp.src(sourcePaths.images, {base: buildDir, cwd: buildDir})
         .pipe(imagemin())
-        .pipe(gulp.dest(distDir));
+        .pipe(gulp.dest(buildDir));
 });
 
-// Default task producing a ready-to-ship frontend in the dist folder
-gulp.task('default', ['_javascripts', '_images']);
+// Minify html
+gulp.task('_html', ['jekyll-build'], function () {
+    return gulp.src(sourcePaths.html, {base: buildDir, cwd: buildDir})
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(minifyInline())
+        .pipe(gulp.dest(buildDir));
+});
+
+// Minify css
+gulp.task('_css', ['jekyll-build'], function () {
+    return gulp.src(sourcePaths.css, {base: buildDir, cwd: buildDir})
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(buildDir));
+});
+
+// Default task producing a ready-to-ship frontend in the build folder
+gulp.task('default', ['jekyll-build', '_javascripts', '_images', '_html', '_css']);
 
 // Check code style on JS
 gulp.task('eslint', function () {
     gulp.src(sourcePaths.javascripts, {cwd: 'src/'})
         .pipe(eslint())
         .pipe(eslint.format());
-});
-
-// Run Jekyll
-gulp.task('jekyll', ['default'], function (gulpCallBack) {
-    let spawn = require('child_process').spawn;
-    let jekyll = spawn('jekyll', ['serve'], {stdio: 'inherit'});
-
-    jekyll.on('exit', function (code) {
-        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
-    });
 });
