561337dd3fa3d674fe43eb8fb0bfdaf101cf105d
Kilian Batzner <git@kilians.net>
2019-11-20 16:36:34 +0100

    Update publish scripts [WIP]

--- util/publish_master_stage.sh
+++ util/publish_master_stage.sh
@@ -4,15 +4,15 @@ TMPDIR=$1
 SITEDIR=$2
 
 cd $SITEDIR
-cp $TMPDIR/util/master-gitignore $SITEDIR/.gitignore
 git checkout master
+cp $TMPDIR/util/master-gitignore $SITEDIR/.gitignore
 # Remove all tracked files (from https://superuser.com/questions/442625/git-delete-all-tracked-files)
 git ls-files -z | xargs -0 rm -f
 # Remove all tracked directories that are empty
 git ls-tree --name-only -d -r -z HEAD | sort -rz | xargs -0 rmdir
 
 # Copy the files
-cp $TMPDIR/_site/* $SITEDIR
+cp -r $TMPDIR/_site/* $SITEDIR
 cp $TMPDIR/util/master-readme.md $SITEDIR/README.md
 touch $SITEDIR/.nojekyll
 
